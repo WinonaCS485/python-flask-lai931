166ee977ad7203cfe762fc27443d96b21f79867b
lai931 <elden.lai@my.rctc.edu>
2020-02-04 13:06:07 -0600

    finished assignment
    got table to show up on site and specified a port

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+import pymysql.cursors
 
+# Connect to the database
+connection = pymysql.connect(host='mrbartucz.com',
+                                 user='gw2246fx',
+                                 password='9Astronaut',
+                                 db='gw2246fx_university',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    
+try:
+    with connection.cursor() as cursor:
+        #user_input = input("enter a student to search for: ")
+        # Select all Students
+        sql = "SELECT * from Student"
+        
+        # execute the SQL command
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        
+finally:
+    connection.close()
+        
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -14,40 +37,17 @@ def cakes():
 def hello(name):
     return render_template('page.html', name=name)
 
-@app.route('database')
+@app.route('/database')
 def database():
-    import pymysql.cursors
-
-    # Connect to the database
-    connection = pymysql.connect(host='mrbartucz.com',
-                                 user='gw2246fx',
-                                 password='9Astronaut',
-                                 db='gw2246fx_university',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    try:
-        with connection.cursor() as cursor:
-            #user_input = input("enter a student to search for: ")
-            # Select all Students
-            sql = "SELECT * from Student"
-            
-            # execute the SQL command
-            cursor.execute(sql)
+    
+            return render_template('database.html', data=data)
             
             # get the results
-            for result in cursor:
-                print (result)
+            #for result in cursor:
             
-          
-            # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
-            # So you must commit to save your changes. 
-            # connection.commit()
-            
-
-    finally:
-        connection.close()
+    
+        
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='2246')
     
